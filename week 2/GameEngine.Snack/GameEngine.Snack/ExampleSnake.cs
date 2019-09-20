68ca0e3e92966767336376fc87bf21933c72ea48
Danila <xawdxawdx@mail.ru>
2019-09-20 19:51:53 +0300

    Сделяль
    Добавил проверочки на то, что еда заспавнится позади змеи, но т.к назад она ходить не может по условию, то просто поворачиваю её влево/право,вверх/вниз.

--- week 2/GameEngine.Snack/GameEngine.Snack/ExampleSnake.cs
+++ week 2/GameEngine.Snack/GameEngine.Snack/ExampleSnake.cs
@@ -1,4 +1,4 @@
-﻿using PixelEngine;
+using PixelEngine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,31 +12,113 @@ namespace GameEngine.Snack
         private const int _width = 50;
         private const int _wallDistanceThreshold = 5;
         private Point _myHeadPosition;
+        private Point _foodPosition;
 
         public string Name => "Example Snake";
 
         public SnakeDirection GetNextDirection(SnakeDirection currentDirection)
         {
-            if(currentDirection == SnakeDirection.Up 
-                && _myHeadPosition.Y < _wallDistanceThreshold)
+            if (_myHeadPosition.Y == _foodPosition.Y)
+            {
+                if (_myHeadPosition.X > _foodPosition.X)
+                {
+                    if (currentDirection == SnakeDirection.Right)
+                    {
+                        return SnakeDirection.Up;
+                    }
+                    else return SnakeDirection.Left;
+                }
+                else if (_myHeadPosition.X < _foodPosition.X)
+                {
+                    if (currentDirection == SnakeDirection.Left)
+                    {
+                        return SnakeDirection.Down;
+                    }
+                    else return SnakeDirection.Right;
+                }
+            }
+            
+            if (_myHeadPosition.X == _foodPosition.X)
+            {
+                if (_myHeadPosition.Y > _foodPosition.Y)
+                {
+                    if (currentDirection == SnakeDirection.Down)
+                    {
+                        return SnakeDirection.Left;
+                    }
+                    else return SnakeDirection.Up;
+                }
+                else if (_myHeadPosition.Y < _foodPosition.Y)
+                {
+                    if (currentDirection == SnakeDirection.Up)
+                    {
+                        return SnakeDirection.Right;
+                    }
+                    else return SnakeDirection.Down;
+                }
+            }
+
+
+            //
+            if (_myHeadPosition.Y > _foodPosition.Y)
+            {
+                if (_myHeadPosition.X > _foodPosition.X)
+                {
+                    return SnakeDirection.Left;
+                }
+                else
+                if (_myHeadPosition.X < _foodPosition.X)
+                {
+                    return SnakeDirection.Right;
+                }
+            }
+
+            if (_myHeadPosition.Y < _foodPosition.Y)
+            {
+                //if (_myHeadPosition.X < _foodPosition.X)
+                //{
+                 //   return SnakeDirection.Down;
+                //}
+
+                if (_myHeadPosition.X < _foodPosition.X)
+                {
+                    return SnakeDirection.Right;
+                }
+                else
+                if (_myHeadPosition.X > _foodPosition.X)
+                {
+                    return SnakeDirection.Left;
+                }
+            }
+
+            if (_myHeadPosition.Y > _foodPosition.Y)
+            {
+                if (_myHeadPosition.X > _foodPosition.X)
+                {
+                    return SnakeDirection.Up;
+                }
+            }
+            //
+            if (currentDirection == SnakeDirection.Up 
+                && _myHeadPosition.Y < _wallDistanceThreshold - 3)
             {
                 return SnakeDirection.Left;
             }
 
             if(currentDirection == SnakeDirection.Right
-                && _myHeadPosition.X > _width - _wallDistanceThreshold)
+                && _myHeadPosition.X > _width - _wallDistanceThreshold + 3)
             {
                 return SnakeDirection.Up;
             }
 
             if(currentDirection == SnakeDirection.Down
-                && _myHeadPosition.Y > _width - _wallDistanceThreshold)
+                && _myHeadPosition.Y > _width - _wallDistanceThreshold + 2)
             {
                 return SnakeDirection.Right;
             }
 
             if (currentDirection == SnakeDirection.Left
-                && _myHeadPosition.X <  _wallDistanceThreshold)
+                && _myHeadPosition.X <  _wallDistanceThreshold - 3)
             {
                 return SnakeDirection.Down;
             }
@@ -47,12 +129,19 @@ namespace GameEngine.Snack
         public void UpdateMap(string map)
         {
             _myHeadPosition = getMyHeadPosition(map);
+            _foodPosition = getFoodPosition(map);
         }
 
         private Point getMyHeadPosition(string map)
         {
             var headIndex = map.IndexOf('*');
             return new Point(headIndex % _width, headIndex / _width);
+        }
+
+        private Point getFoodPosition(string map)
+        {
+            var foodIndex = map.IndexOf('7');
+            return new Point(foodIndex % _width, foodIndex / _width);
         }
     }
 }
